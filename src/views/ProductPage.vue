c7f1411b2fb93241b45621d0eaa580a85f499a8f
Lars Rosenström <lars.rosenstrom@kb.se>
2023-04-13 11:42:29 +0200

    Read data for product page

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,12 +1,32 @@
-
 <script>
+
+import { getDocument } from '@/lib/http';
+
 export default {
-    name: "ProductPage.vue"
+    name: "ProductPage.vue",
+    data() {
+        return {
+            documentData: null,
+            documentSummary: null
+        };
+    },
+    computed: {
+        recordId() {
+            return this.$route.params.fnurgel;
+        },
+    },
+    mounted() {
+        getDocument(`${this.recordId}/data.jsonld?embellished=false`).then((res) => {
+            console.log('getDocument', res);
+            this.documentData = res.data['@graph'][0];
+            this.documentSummary = res.data['@graph'][1];
+        });
+    }
 }
 </script>
 
 <template>
 <div>
-    Testing
+    {{this.documentData}}
 </div>
 </template>
