c0d9a1f95e82f2679bafc5d4f7273cf6837c93f8
Andreas Ekström <didair@msn.com>
2023-04-17 16:54:05 +0200

    displayGroups => groups

--- src/stores/display.js
+++ src/stores/display.js
@@ -4,13 +4,13 @@ import { getDisplayDefinitions } from "../lib/data";
 export const useDisplayStore = defineStore('display', {
 	state: () => ({
 		current: null,
-		displayGroups: null,
+		groups: null,
 	}),
 	actions: {
 		async _init() {
 			const response = await getDisplayDefinitions();
 			this.current = response;
-			this.displayGroups = await fetch('/json/displayGroups.json').then((response) =>
+			this.groups = await fetch('/json/displayGroups.json').then((response) =>
 				response.json()
 			);
 		},
