39ebf37578a44d0e44ba894d88ec0809c8d2c07c
Andreas Ekström <didair@msn.com>
2023-04-18 20:07:20 +0200

    Fix loader

--- src/piniaPlugin.js
+++ src/piniaPlugin.js
@@ -6,8 +6,12 @@ export function initStore(context) {
 	if (context.store._init != null && typeof context.store._init === 'function') {
 		const promise = context.store._init();
 
-		if (promise.then != null) {
+		if (promise != null && promise.then != null) {
 			loader.pendingPromises++;
+			if (loader.pendingPromises > loader.highest) {
+				loader.highest = loader.pendingPromises;
+			}
+
 			promise.then(() => {
 				loader.pendingPromises--;
 			});
--- src/stores/loader.js
+++ src/stores/loader.js
@@ -3,8 +3,9 @@ import { defineStore } from "pinia";
 export const useLoaderStore = defineStore('loader', {
 	state: () => ({
 		pendingPromises: 0,
+		highest: 0,
 	}),
 	getters: {
-		isLoading: (state) => state.pendingPromises > 0,
+		isLoading: (state) => state.pendingPromises > 0 || state.highest == 0,
 	},
 });
