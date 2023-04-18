2938dcbfc380abf54ca211732af8d8cef3bf9e48
Andreas Ekström <didair@msn.com>
2023-04-18 20:08:06 +0200

    Use lxljs in SearchResults store

--- src/views/SearchResults/store.js
+++ src/views/SearchResults/store.js
@@ -1,29 +1,12 @@
 import { defineStore } from "pinia";
-import { getItemById } from "@/lib/item";
-import { getItemsByRelation } from "../../lib/item";
+import { getChip, getItemSummary, getItemLabel } from "@/lxljs/display";
+import { getResources } from "@/lib/resources";
+import settings from "@/lib/settings";
 import { getRelatedRecords } from '@/lib/http';
-import { useVocabStore } from "../../stores/vocab";
 
 export const useDataStore = defineStore('data', {
 	state: () => ({
 		current: null,
-		AudioBook: [],
-		Book: [],
-		DigitalReproduction: [],
-		EBook: [],
-		GenreForm: [],
-		Language: [],
-		Manuscript: [],
-		Movie: [],
-		Organization: [],
-		Performance: [],
-		Person: [],
-		Play: [],
-		PrintedVolume: [],
-		SpokenBook: [],
-		Topic: [],
-		VideoRecording: [],
-		WorkConcept: [],
 
 		Instance: [],
 		Print: [],
@@ -51,55 +34,27 @@ export const useDataStore = defineStore('data', {
 		},
 	},
 	actions: {
-		async _init() {
-			const vocabStore = useVocabStore();
-			const response = await getRelatedRecords({
-				'q': '*',
-				'_limit': 20,
-				'@type': 'Work',
-				'_sort': '',
-			}, 'http://kblocalhost.kb.se:5000');
-
-			this.current = response.items;
-			this.memoIndexData();
-
-			vocabStore.$subscribe((mutation) => {
-				if (mutation.type === 'direct') {
-					Object.keys(this.$state).forEach((stateKey) => {
-						if (stateKey === 'graph' || stateKey === 'current' || stateKey === 'context') {
-							return false;
-						}
-
-						if (Array.isArray(this.$state[stateKey])) {
-							this.$state[stateKey] = [];
-						}
-					});
-
-					this.memoIndexData();
-				}
-			});
-		},
-
 		async query(values = {}) {
-			if (values.q === '') {
+			if (values.q == null || values.q == '') {
 				values.q = '*';
 			}
 
+			console.log('run query......', values);
+
 			const response = await getRelatedRecords({
 				'_limit': 20,
-				'@type': 'Work',
+				'@type': 'Text',
 				'_sort': '',
 				...values,
 			}, 'http://kblocalhost.kb.se:5000');
 
 			this.$reset();
 
-			console.log('query response', response);
 			this.current = response.items;
-			this.memoIndexData();
+			this.indexData();
 		},
 
-		memoIndexData() {
+		indexData() {
 			if (this.current == null) {
 				return false;
 			}
@@ -107,91 +62,36 @@ export const useDataStore = defineStore('data', {
 			const stateKeys = Object.keys(this.$state);
 
 			this.current.forEach((item) => {
-				if (stateKeys.indexOf(item['@type']) > -1) {
-					this.$state[item['@type']].push(this.expressItem(item));
+				if (stateKeys.indexOf(item['@type']) > -1 && Array.isArray(this.$state[item['@type']])) {
+					if (getResources().context != null) {
+						const chip = getChip(item, getResources(), [], settings);
+						const summary = getItemSummary(
+							item,
+							getResources(),
+							[],
+							settings,
+							getResources().displayGroups,
+						);
+
+						this.$state[item['@type']].push({
+							...item,
+							...summary,
+							...chip,
+						});
+					}
 				}
 			});
 
-			stateKeys.forEach((stateKey) => {
-				if (Array.isArray(this.$state[stateKey])) {
-					this.$state[stateKey] = this.$state[stateKey].map(this.calculateDisplayMeta);
-				}
+			stateKeys.forEach((key) => {
+				this.$state[key] = this.$state[key].map(this.calculateDisplayMeta);
 			});
-		},
-
-		expressItem(item) {
-			const clone = JSON.parse(JSON.stringify(item));
-
-			if (this.current == null) {
-				return null;
-			}
-
-			const expressObject = (item) => {
-				let result = JSON.parse(JSON.stringify(item));
-				if (item.hasOwnProperty('@id')) {
-					result = getItemById(item['@id']);
-				}
-
-				return result ?? item;
-			};
-
-			const diveInKey = (_item, key) => {
-				const item = JSON.parse(JSON.stringify(_item));
-				if (Array.isArray(item[key])) {
-					return item[key].map((subItem) => {
-						if (Array.isArray(subItem)) {
-							return diveInKey(subItem, key);
-						}
-
-						if (typeof subItem === 'object') {
-							return expressObject(subItem);
-						}
-					});
-				}
-
-				if (typeof item[key] === 'object') {
-					return expressObject(item[key]);
-				}
-
-				return item[key];
-			};
-
-			Object.keys(clone).forEach((itemKey) => {
-				clone[itemKey] = diveInKey(clone, itemKey);
-			});
-
-			return clone;
-		},
-
-		buildRelations(item) {
-			if (item['@type'] === 'Book') {
-				const foundInstances = getItemsByRelation(item['@id'], 'instanceOf');
-				if (foundInstances.length > 0) {
-					item['instances'] = foundInstances;
-				}
-			}
-
-			if (item['@type'] === 'WorkConcept') {
-				const foundExpressions = getItemsByRelation(item['@id'], 'expressionOf');
-				if (foundExpressions.length > 0) {
-					item['expressions'] = foundExpressions;
-				}
-			}
-
-			return item;
 		},
 
 		calculateDisplayMeta(item) {
 			const clone = JSON.parse(JSON.stringify(item));
 
 			if (item['@type'] === 'Text') {
-				const foundTitle = clone.hasTitle?.find((title) =>
-					title['@type'] === 'Title'
-				);
-
-				if (foundTitle != null) {
-					clone.title = foundTitle.mainTitle;
-				}
+				clone.title = getItemLabel(item.hasTitle[0], getResources(), [], settings);
 			}
 
 			return clone;
