1a44356f542800feb9d856dc0b83b70532962396
Andreas Ekström <didair@msn.com>
2023-04-26 11:57:56 +0200

    Update style on checkbox

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -41,14 +41,59 @@ export default {
 
 <template>
 	<label class="wrapper flex items-center relative cursor-pointer">
-		<input class="checkbox" type="checkbox" :checked="isChecked" :value="value" @change="updateInput"/>
-		<span class="checkmark" />
+		<input
+			class="hidden opacity-0 absolute w-0 h-0"
+			type="checkbox"
+			:checked="isChecked"
+			:value="value"
+			@change="updateInput"
+		/>
+
+		<div className="`
+			checkmark
+			flex
+			items-center
+			justify-center
+			relative
+			h-5 w-5
+			rounded-md
+			border-2
+			border-secondary-turquoise
+			hover:border-secondary-darker-turquoise
+		`" />
 
 		<span className="inline-block ml-2">
-			{{label}}
+			{{ label }}
 		</span>
 	</label>
 </template>
 
 <style lang="css" scoped>
+.wrapper input:checked~.checkmark {
+	@apply bg-secondary-turquoise;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+	content: "";
+	@apply hidden;
+}
+
+/* Show the checkmark when checked */
+.wrapper input:checked~.checkmark:after {
+	@apply block;
+}
+
+/* Style the checkmark/indicator */
+.wrapper .checkmark:after {
+	position: relative;
+	top: -1px;
+	width: 6px;
+	height: 12px;
+	border: solid white;
+	border-width: 0 3px 3px 0;
+	-webkit-transform: rotate(45deg);
+	-ms-transform: rotate(45deg);
+	transform: rotate(45deg);
+}
 </style>
