8852c4cfed58bb4d135019d68b750ec9a974bf3c
Andreas Ekström <didair@msn.com>
2023-04-26 10:41:46 +0200

    Add new component:Grid

---
+++ src/components/Grid.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 w-full">
+		<slot />
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'Grid',
+};
+</script>
