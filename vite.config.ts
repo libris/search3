c327e9f530f81263f5b55e88ebe61627445639d4
Anders Jensen-Urstad <anders.jensen-urstad@kb.se>
2023-04-27 16:57:38 +0200

    Use VITE_VUE_APP_BASE_URL also in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_VUE_APP_BASE_URL || '/',
   plugins: [vue()],
   resolve: {
     alias: {
