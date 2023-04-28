10f5fcba360185bc3c66dab42b0c698cd233f6d8
Andreas Ekström <didair@msn.com>
2023-04-28 11:08:12 +0200

    Add Select component

---
+++ src/components/Select.vue
@@ -0,0 +1,20 @@
+<template>
+	<select
+		v-bind="[$props, $attrs]"
+		class="`
+			border
+			border-secondary-grey
+			py-2 px-4
+			rounded-md
+			bg-primary-white
+		`"
+	>
+		<slot />
+	</select>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'Select',
+};
+</script>
