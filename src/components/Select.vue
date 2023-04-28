3f0b930eb76f2ea814cdc6ba4b0a70769843f53a
Andreas Ekström <didair@msn.com>
2023-04-28 14:04:46 +0200

    Fix select input

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,6 +1,8 @@
 <template>
 	<select
-		v-bind="[$props, $attrs]"
+		v-model="selected"
+		:disabled="disabled"
+		@change="handleChange"
 		class="`
 			border
 			border-secondary-grey
@@ -8,6 +10,7 @@
 			rounded-md
 			bg-primary-white
 		`"
+		:class="class"
 	>
 		<slot />
 	</select>
@@ -16,5 +19,36 @@
 <script lang="ts">
 export default {
 	name: 'Select',
+	props: {
+		value: {
+			type: [String, Number],
+			default: '',
+		},
+		class: {
+			type: String,
+			default: '',
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data() {
+		return {
+			selected: this.value,
+		};
+	},
+	methods: {
+		handleChange(event) {
+			this.selected = event.target.value;
+			this.$emit('update:value', this.selected);
+			this.$emit('change', this.selected);
+		},
+	},
+	watch: {
+		value(newValue) {
+			this.selected = newValue;
+		},
+	},
 };
 </script>
