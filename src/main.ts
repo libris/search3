bcecb6d91fa17e947a2f4f593db556575aca5fab
Andreas Ekström <didair@msn.com>
2023-05-08 09:44:16 +0200

    Add vue-final-modal

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { initStore } from './piniaPlugin';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createVfm } from 'vue-final-modal';
 import router from '@/lib/router';
 import './lib/icons';
 import App from './App.vue';
 
+const vfm = createVfm();
 const pinia = createPinia();
 const app = createApp(App);
 
 pinia.use(initStore);
 app.use(pinia);
 app.use(router);
+app.use(vfm);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
