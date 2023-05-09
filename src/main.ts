8fe9bf5af7778daa74eb0ea08a4e79fcaaa6a389
Andreas Ekström <didair@msn.com>
2023-05-09 11:23:55 +0200

    Make components Button and Card globally available

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { createPinia } from 'pinia';
 import { initStore } from './piniaPlugin';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createVfm } from 'vue-final-modal';
+import 'vue-final-modal/style.css'
 import router from '@/lib/router';
 import './lib/icons';
 import App from './App.vue';
+
+import Button from './components/Button.vue';
+import Card from './components/Card.vue';
 
 const vfm = createVfm();
 const pinia = createPinia();
@@ -15,5 +19,9 @@ pinia.use(initStore);
 app.use(pinia);
 app.use(router);
 app.use(vfm);
+
+app.component('Button', Button);
+app.component('Card', Card);
+
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
