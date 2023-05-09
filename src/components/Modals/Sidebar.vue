41011d37d5fd10af473fa12f0752609e0d68c15f
Andreas Ekström <didair@msn.com>
2023-05-09 11:36:11 +0200

    Add responsive styling for sidebarmodal and a close button if full width

--- src/components/Modals/Sidebar.vue
+++ src/components/Modals/Sidebar.vue
@@ -1,18 +1,37 @@
 <script lang="ts">
 import { VueFinalModal } from 'vue-final-modal';
+import { useVfm } from 'vue-final-modal';
+
 export default {
 	name: 'SidebarModal',
 	components: {
 		VueFinalModal,
+	},
+	data: () => ({
+		modalId: Symbol('modalId'),
+	}),
+	methods: {
+		closeModal() {
+			const vfm = useVfm();
+			vfm.close(this.modalId);
+		},
 	},
 };
 </script>
 
 <template>
 	<VueFinalModal
+		:modal-id="modalId"
 		class="flex justify-end items-start"
-		content-class="flex flex-col max-w-xl p-4 bg-primary-white w-2/5 h-full"
+		content-class="flex flex-col p-4 bg-primary-white w-full lg:w-2/5 h-full"
 	>
+		<div class="flex lg:hidden justify-end mb-4 text-3xl">
+			<font-awesome-icon
+				icon="fa-solid fa-xmark"
+				@click="closeModal"
+			/>
+		</div>
+
 		<slot />
 	</VueFinalModal>
 </template>
