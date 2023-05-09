9ba494b09318b71a52d0bf6abd45968ea7d770dd
Andreas Ekström <didair@msn.com>
2023-05-09 11:23:16 +0200

    Add modal component: Sidebar

---
+++ src/components/Modals/Sidebar.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { VueFinalModal } from 'vue-final-modal';
+export default {
+	name: 'SidebarModal',
+	components: {
+		VueFinalModal,
+	},
+};
+</script>
+
+<template>
+	<VueFinalModal
+		class="flex justify-end items-start"
+		content-class="flex flex-col max-w-xl p-4 bg-primary-white w-2/5 h-full"
+	>
+		<slot />
+	</VueFinalModal>
+</template>
