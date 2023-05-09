c9be01e1ef5e8e3fb20a600d31d9ff422c65c45a
Andreas Ekström <didair@msn.com>
2023-05-09 11:23:24 +0200

    Add basic button component with a general style

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+	name: 'Button',
+};
+</script>
+
+<template>
+	<button
+		className="rounded-full py-2 px-4 inline-flex items-center cursor-pointer"
+		style="color: #2440bc; background: #e1e5f6;"
+	>
+		<slot />
+	</button>
+</template>
