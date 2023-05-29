1420210c11187394cf12fe633924c82c74b5fbbf
Andreas Ekström <didair@msn.com>
2023-05-29 14:40:47 +0200

    Add BookmarkButton on WorkSummary

--- src/views/ProductPage/Work/WorkSummary.vue
+++ src/views/ProductPage/Work/WorkSummary.vue
@@ -5,9 +5,13 @@ import { getItemLabel } from "@/lxljs/display";
 import { getResources } from "@/lib/resources";
 import settings from '@/lib/settings';
 import { getPropertyLabel } from "@/lib/item";
+import BookmarkButton from "@/components/BookmarkButton.vue";
 
 export default {
     name: "WorkSummary",
+    components: {
+        BookmarkButton,
+    },
     computed: {
 		...mapState(useProductStore, {
 			workData: 'mainEntity',
@@ -81,19 +85,26 @@ export default {
         </div>
 
         <div v-if="genreForm && genreForm.length != 0" class="font-bold text-secondary-turquoise mt-3 mb-1">{{ gfLabel }}</div>
-            <div v-for="(scheme, index) in gfSchemes">
-              <div class="text-secondary-grey text-base font-medium"> {{ scheme.scheme }} </div>
-                <div class="flex flex-wrap gap-2 pb-1">
-                  <span v-for="gf in scheme.genreForms">
-                    <router-link v-if="gf.link" :to="gf.link" class="text-s text-secondary-turquoise underline">{{gf.genreForm}}</router-link>
-                    <span v-else class="text-s text-secondary-turquoise">{{gf.genreForm}}</span>
-                  </span>
-                </div>
-            </div>
 
-        <div v-if="subjects && subjects.length != 0" class="font-bold text-secondary-turquoise mt-3">{{ subjectLabel }}</div>
+        <div v-for="(scheme, index) in gfSchemes">
+            <div class="text-secondary-grey text-base font-medium">{{ scheme.scheme }}</div>
+
+            <div class="flex flex-wrap gap-2 pb-1">
+                <span v-for="gf in scheme.genreForms">
+                    <router-link v-if="gf.link" :to="gf.link" class="text-s text-secondary-turquoise underline">
+                        {{gf.genreForm}}
+                    </router-link>
+                    <span v-else class="text-s text-secondary-turquoise">{{gf.genreForm}}</span>
+                </span>
+            </div>
+        </div>
+
+        <div v-if="subjects && subjects.length != 0" class="font-bold text-secondary-turquoise mt-3">
+            {{ subjectLabel }}
+        </div>
+
         <div v-for="(scheme, index) in subjectSchemes">
-          <span class="text-secondary-grey text-base font-medium"> {{ scheme.scheme }} </span>
+            <span class="text-secondary-grey text-base font-medium"> {{ scheme.scheme }} </span>
             <div class="flex flex-wrap gap-2 pb-1">
               <div v-for="s in scheme.subjects">
                 <router-link v-if="s.link" :to="s.link" class="text-s text-secondary-turquoise underline">{{s.subject}}</router-link>
@@ -101,8 +112,15 @@ export default {
               </div>
             </div>
         </div>
+
         <div class="max-w-2xl py-3" v-if="summary != null">
             {{ summary }}
         </div>
+
+        <template #footer>
+            <div class="flex items-end justify-end border-t border-t-secondary-grey/20 pt-4">
+                <BookmarkButton :id="workData['@id']" />
+            </div>
+        </template>
     </Card>
 </template>
