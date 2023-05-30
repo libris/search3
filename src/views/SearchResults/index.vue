a2ecf363049ce1c76547b4c2c7bf118fa88b44ef
Andreas Ekström <didair@msn.com>
2023-05-30 14:07:18 +0200

    If screen is narrower than md, display facets in SidebarModal

--- src/views/SearchResults/index.vue
+++ src/views/SearchResults/index.vue
@@ -1,15 +1,21 @@
 <script lang="ts">
-import { getQueryParams } from '@/lib/http';
 import Facets from '@/components/Facets.vue';
 import Query from './Query.vue';
 import PaginationControls from '@/components/PaginationControls.vue';
+import SidebarModal from '@/components/Modals/Sidebar.vue';
+import SelectedFacets from '@/components/SelectedFacets.vue';
 
 export default {
 	components: {
 		Facets,
 		Query,
-		PaginationControls
+		PaginationControls,
+		SidebarModal,
+		SelectedFacets
 	},
+	data: () => ({
+		facetModal: false,
+	}),
 	watch: {
 		'$route.fullPath'() {
 			document.documentElement.scrollTo(0, 0);
@@ -20,7 +26,7 @@ export default {
 
 <template>
 	<div class="flex gap-x-10">
-		<div class="w-1/4">
+		<div class="hidden md:block w-1/4">
 			<h3 class="text-2xl text-secondary-grey font-semibold mb-2">
 				Förfina urvalet
 			</h3>
@@ -28,7 +34,18 @@ export default {
 			<Facets />
 		</div>
 
-		<div class="w-3/4">
+		<div class="w-full md:w-3/4">
+			<div class="md:hidden mb-4">
+				<Button @click="() => this.facetModal = !this.facetModal">
+					<font-awesome-icon icon="fa-solid fa-filter" class="mr-2" />
+					Visa facetter
+				</Button>
+
+				<div class="mt-4">
+					<SelectedFacets />
+				</div>
+			</div>
+
 			<Query />
 
 			<div class="mt-6 pt-6 border-t border-t-secondary-grey/20">
@@ -36,4 +53,12 @@ export default {
 			</div>
 		</div>
 	</div>
+
+	<SidebarModal v-model="facetModal">
+		<h3 class="text-2xl text-secondary-grey font-semibold mb-2">
+			Förfina urvalet
+		</h3>
+
+		<Facets />
+	</SidebarModal>
 </template>
